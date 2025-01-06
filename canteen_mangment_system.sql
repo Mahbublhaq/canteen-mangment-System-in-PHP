785b1776ae43ad0bfe1dd85c9caa0fdfca690a8b
Mahbubul Haq <mahbubulhaq.cu.05@gmail.com>
2025-01-06 15:39:53 +0600

    Final DB

--- canteen_mangment_system.sql
+++ canteen_mangment_system.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2024 at 04:30 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Jan 06, 2025 at 10:38 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,7 +43,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `phone`, `profile_picture`, `role`, `created_at`) VALUES
-(1, 'Mahabub', 'admin@gmail.com', '$2y$10$Sw5q.tN6ari2Qu4aCp9AROXjrxmieS2DKfr6RACA4ZifoDQftyzKy', '01601337085', 'uploads/1734372046_111111111111111111.png', 'admin', '2024-11-18 13:52:36');
+(1, 'Mahabub', 'admin@gmail.com', '$2y$10$Sw5q.tN6ari2Qu4aCp9AROXjrxmieS2DKfr6RACA4ZifoDQftyzKy', '01601337085', 'uploads/1735459334_Mahabub.d.png', 'admin', '2024-11-18 13:52:36');
 
 -- --------------------------------------------------------
 
@@ -62,6 +62,14 @@ CREATE TABLE `contact_queries` (
   `status` varchar(255) NOT NULL DEFAULT 'Pending',
   `reply_message` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact_queries`
+--
+
+INSERT INTO `contact_queries` (`id`, `name`, `email`, `problem`, `details`, `query`, `created_at`, `status`, `reply_message`) VALUES
+(2, 'Mahbubul Haq', 'mahabubpanti@gmail.com', 'Deposit Problem', '', 'I Have facing Desposit problem. Please help Me', '2024-12-29 08:18:05', 'confirmed', 'Which kind of problem are you facting please tell me in details'),
+(3, 'Shaikh Mahbubul Huq', 'mahbubulhaq.cu.05@gmail.com', 'Deposit Problem', '', 'Deposit Problem Please Help', '2024-12-29 09:13:25', 'confirmed', 'hi');
 
 -- --------------------------------------------------------
 
@@ -84,6 +92,13 @@ CREATE TABLE `customers` (
   `signup_method` enum('regular','google') DEFAULT 'regular'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `customer_name`, `email`, `password`, `phone`, `address`, `created_at`, `profile_picture`, `delivery_address`, `delivery_notice`, `delivery_mobile`, `signup_method`) VALUES
+(1, 'Shaikh Mahbubul Huq', 'mahbubulhaq.cu.05@gmail.com', '$2y$10$Sa4dfNMdlYJX5zYYF/QYnOOuJ9IRf3OhJGMn/9ID3plb4dYbJSRkq', '01601337085', 'City University', '2025-01-06 03:41:58', '1_profile_677b9815e9dc5.png', 'Mirpur 14', 'None', '01718337085', 'google');
+
 -- --------------------------------------------------------
 
 --
@@ -98,6 +113,39 @@ CREATE TABLE `deposit_history` (
   `deposit_amount` decimal(10,2) NOT NULL,
   `transaction_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `deposit_history`
+--
+
+INSERT INTO `deposit_history` (`id`, `customer_id`, `payment_method`, `payment_details`, `deposit_amount`, `transaction_date`) VALUES
+(8, 37, 'Bkash', 'First Deposit ', 2000.00, '2024-12-29 09:22:13'),
+(9, 38, 'Bkash', 'First Deposit ', 2000.00, '2025-01-05 18:19:10'),
+(10, 1, 'Bkash', 'First Deposit ', 2000.00, '2025-01-06 08:46:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest_customer`
+--
+
+CREATE TABLE `guest_customer` (
+  `id` int(11) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `total_orders` int(11) DEFAULT 0,
+  `total_spent` decimal(10,2) DEFAULT 0.00,
+  `last_order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `order_details` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `phone_number` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guest_customer`
+--
+
+INSERT INTO `guest_customer` (`id`, `customer_name`, `total_orders`, `total_spent`, `last_order_date`, `order_details`, `created_at`, `phone_number`) VALUES
+(3, 'Mahabub', 1, 200.00, '2025-01-06 03:06:18', '[\"[{\\\"product_id\\\":11,\\\"product_name\\\":\\\"Kacchi Biryani\\\",\\\"quantity\\\":1,\\\"price\\\":\\\"200.00\\\",\\\"subtotal\\\":200}]\"]', '2025-01-06 08:06:18', '01601337085');
 
 -- --------------------------------------------------------
 
@@ -121,6 +169,13 @@ CREATE TABLE `meal` (
   `status` enum('active','inactive') DEFAULT 'active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `meal`
+--
+
+INSERT INTO `meal` (`id`, `meal_id`, `lunch_meal`, `dinner_meal`, `deposit`, `meal_price`, `remain_balance`, `active`, `created_at`, `lunch_quantity`, `dinner_quantity`, `total_deposits`, `status`) VALUES
+(1, 1, 0, 0, 2000.00, NULL, 2000.00, 1, '2025-01-06 18:00:00', NULL, NULL, 0.00, 'active');
+
 -- --------------------------------------------------------
 
 --
@@ -136,8 +191,17 @@ CREATE TABLE `meal_registration` (
   `varsity_id` varchar(50) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `active` tinyint(2) NOT NULL DEFAULT 0,
-  `meal_date` date NOT NULL
+  `meal_date` date NOT NULL,
+  `id_card_front` varchar(255) NOT NULL,
+  `id_card_back` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `meal_registration`
+--
+
+INSERT INTO `meal_registration` (`id`, `customer_id`, `name`, `department`, `deposit`, `varsity_id`, `email`, `active`, `meal_date`, `id_card_front`, `id_card_back`) VALUES
+(1, 1, 'Shaikh Mahbubul Huq', 'Computer Science', 2000.00, '1925102005', 'mahbubulhaq.cu.05@gmail.com', 1, '2025-01-07', '../uploads/id_cards/677b97c127503.jpg', '../uploads/id_cards/677b97c131eeb.jpg');
 
 -- --------------------------------------------------------
 
@@ -155,6 +219,14 @@ CREATE TABLE `offers` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `offers`
+--
+
+INSERT INTO `offers` (`id`, `offer_name`, `discount_code`, `discount_amount`, `expiry_date`, `created_at`, `updated_at`) VALUES
+(4, 'Hot Offer', 'mahabub', 50, '2024-12-30', '2024-12-29 08:24:50', '2024-12-29 08:24:50'),
+(5, 'Hot Offer', 'Mahabub', 50, '2025-01-08', '2025-01-06 09:13:49', '2025-01-06 09:13:49');
+
 -- --------------------------------------------------------
 
 --
@@ -163,6 +235,7 @@ CREATE TABLE `offers` (
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
+  `gest_customer_id` int(20) DEFAULT 0,
   `customer_id` int(11) NOT NULL,
   `order_details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `total_cost` decimal(10,2) NOT NULL,
@@ -170,12 +243,19 @@ CREATE TABLE `orders` (
   `discount_amount` decimal(10,2) NOT NULL,
   `net_total` decimal(10,2) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `payment_method` enum('Cash on Delivery','Online') NOT NULL,
+  `payment_method` enum('Cash on Delivery','Online','ShoptoSell') NOT NULL,
   `discount_code` varchar(20) DEFAULT NULL,
   `order_status` varchar(255) NOT NULL DEFAULT 'Pending',
   `admin_name` varchar(255) NOT NULL,
   `admin_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `gest_customer_id`, `customer_id`, `order_details`, `total_cost`, `subtotal`, `discount_amount`, `net_total`, `created_at`, `payment_method`, `discount_code`, `order_status`, `admin_name`, `admin_id`) VALUES
+(4, 3, 0, '[{\"product_id\":11,\"product_name\":\"Kacchi Biryani\",\"quantity\":1,\"price\":\"200.00\",\"subtotal\":200}]', 200.00, 200.00, 0.00, 200.00, '2025-01-06 08:06:18', 'ShoptoSell', NULL, 'Confirmed', 'Mahabub', 1);
 
 -- --------------------------------------------------------
 
@@ -242,6 +322,13 @@ ALTER TABLE `deposit_history`
   ADD KEY `customer_id` (`customer_id`);
 
 --
+-- Indexes for table `guest_customer`
+--
+ALTER TABLE `guest_customer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phone_number` (`phone_number`);
+
+--
 -- Indexes for table `meal`
 --
 ALTER TABLE `meal`
@@ -289,43 +376,49 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `contact_queries`
 --
 ALTER TABLE `contact_queries`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `deposit_history`
 --
 ALTER TABLE `deposit_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `guest_customer`
+--
+ALTER TABLE `guest_customer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `meal`
 --
 ALTER TABLE `meal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `meal_registration`
 --
 ALTER TABLE `meal_registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `offers`
 --
 ALTER TABLE `offers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
